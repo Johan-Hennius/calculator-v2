4b4b1fb8d96661b1fac9bed4ecd1599f317e3959
Johan Hennius <johan.bh19@gmail.com>
2023-10-21 08:53:10 +0000

    Link up js button variables to DOM

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+//linking button variables to DOM
+const numberButtons = document.querySelectorAll(['data-number']);
+const operationButtons = document.querySelectorAll(['data-operation']);
+const equalsButton = document.querySelector(['data-equals']);
+const deleteButton = document.querySelector(['data-delete']);
+const allClearButton = document.querySelector(['data-all-clear']);
+const previousOperandButton = document.querySelector(['data-previous-operand']);
+const currentOperandButton = document.querySelector(['data-current-operand']);
